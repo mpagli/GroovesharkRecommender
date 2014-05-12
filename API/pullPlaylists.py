9113f9d7bfeae7c44905b5d2b364ecf62a7d1cb4
mat <matteo.pagliardini@gmail.com>
2014-05-12 20:36:00 +0200

    pullPlaylists.py and sample json formatted playlists

---
+++ API/pullPlaylists.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import subprocess
+import sys
+import json
+from grooveshark import Client
+
+client = Client()
+client.init()
+
+if len(sys.argv) < 3 or False in (sys.argv[1].isdigit(),sys.argv[2].isdigit()):
+	print "Usage : ./pullPlaylists <startIdx> <endIdx>"
+	sys.exit(2)
+
+idStart = int(sys.argv[1])
+idStop  = int(sys.argv[2]) 
+
+outPath = "./playlists/"
+
+count = 0 	#total number of playlists saved
+
+for id_ in xrange(max(1,idStart),idStop+1):
+	query = client.playlist(str(id_))
+	if id_ == query['PlaylistID']: 	#query['PlaylistID']=0 when the playlist doesn't exists 
+		jsonPath = outPath + str(query['PlaylistID']) + ".json"
+		with open(jsonPath,'w') as stream:
+			json.dump(query, stream)
+		print "\tPlaylist " + str(id_) + " saved as " + jsonPath
+		count += 1
+
+print str(count) + " have been pulled in " + outPath
